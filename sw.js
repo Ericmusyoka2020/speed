ac8426b06233d9d9b0202a7bec5ded7478eb5c12
Ericmusyoka2020 <ericmusyoka17@gmail.com>
2026-02-22 19:24:52 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'cyclestream-v1';
+const assets = [
+  './',
+  './index.html',
+  './logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
